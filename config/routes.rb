34cb7d349be3fc74a91986bde974b1db22cbf0da
Sam Milechman <sammilechman@gmail.com>
2014-04-22 17:56:24 -0700

    Final commit for W4D2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 FirstRoutes::Application.routes.draw do
   resources :users, :only => [:create, :destroy, :index, :show, :update] do
     resources :contacts, :only => [:index]
+    resources :comments, :only =>
   end
 
   resources :contacts, :only => [:create, :destroy, :show, :update]
